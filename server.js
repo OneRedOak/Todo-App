4ff6927505685d69977a79033be208f4d9118291
Patrick Ellis <oneredoak@gmail.com>
2014-02-24 11:31:54 -0800

    Express Routes Added for API Calls to MongleDB

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// Initial Setup
+// Initial Setup -----------------------------------------
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 
-// Configuration
+// Configuration -----------------------------------------
 mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'); // Scotch.io tutorial - test database
 
 app.configure(function() {
@@ -13,6 +13,56 @@ app.configure(function() {
 	app.use(express.methodOverride()); // Simulates DELET and PUT
 });
 
-// Start Node app
+// Models -----------------------------------------
+var Todo = mongoose.model('Todo', {
+	text: String
+});
+
+// Listen - Node app -----------------------------------------
 app.listen(8080);
 console.log('App is listning on port 8080');
+
+// Routes -----------------------------------------
+	// Api ---------------------------------------------------------------------
+	// Get all todos
+	app.get('/api/todos', function(req, res) {
+		// Use mongoose to get all todos in the database
+		Todo.find(function(err, todos) {
+			// If there is an error retrieving, send the error. nothing after res.send(err) will execute
+			if (err)
+				res.send(err)
+			res.json(todos); // Return all todos in JSON format
+		});
+	});
+	// Create todo and send back all todos after creation
+	app.post('/api/todos', function(req, res) {
+		// create a todo, information comes from AJAX request from Angular
+		Todo.create({
+			text: req.body.text,
+			done: false
+		}, function(err, todo) {
+			if (err)
+				res.send(err);
+			// Get and return all the todos after you create another
+			Todo.find(function(err, todos) {
+				if (err)
+					res.send(err)
+				res.json(todos);
+			});
+		});
+	});
+	// Delete a todo
+	app.delete('/api/todos/:todo_id', function(req, res) {
+		Todo.remove({
+			_id : req.params.todo_id
+		}, function(err, todo) {
+			if (err)
+				res.send(err);
+			// Get and return all the todos after you create another
+			Todo.find(function(err, todos) {
+				if (err)
+					res.send(err)
+				res.json(todos);
+			});
+		});
+	});
